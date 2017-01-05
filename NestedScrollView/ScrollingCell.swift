fd992375ffb9630fa1b0d25d94e02aac47fc129a
Zhu Jiang <scorpiozj@gmail.com>
2017-01-05 13:58:28 +0800

    Add init add Collection Cell

---
+++ NestedScrollView/ScrollingCell.swift
@@ -0,0 +1,13 @@
+//
+//  ScrollingCell.swift
+//  NestedScrollView
+//
+//  Created by ZhuJiang on 17/1/5.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+import UIKit
+
+class ScrollingCell: UICollectionViewCell {
+    
+}
