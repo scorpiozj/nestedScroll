3afedbeac82e9f6e844f37d60aab3b03184f0299
Zhu Jiang <scorpiozj@gmail.com>
2017-01-10 14:10:08 +0800

    Add Establish basic structure: 1. parent scrollView 2. two child view: collection view and single View

--- NestedScrollView/ScrollingCell.swift
+++ NestedScrollView/ScrollingCell.swift
@@ -8,6 +8,68 @@
 
 import UIKit
 
-class ScrollingCell: UICollectionViewCell {
+private let kColorViewTag: Int    =   100
+
+open class ScrollingCell: UICollectionViewCell, UIScrollViewDelegate {
+    
+    //MARK:
+    ///default color is red
+    var color: UIColor! = UIColor.red{
+        didSet {
+            self.contentView.backgroundColor = self.color
+        }
+    }
+    weak open var delegate: ScrollingCellDelegate?
+    static var cellIdentifier: String = "ScrollingCellIdentifier"
+    var scrollview: UIScrollView? = nil
+    
+    // MARK:
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        self.contentView.backgroundColor = self.color
+        
+        self.scrollview = UIScrollView.init()
+        self.scrollview!.delegate = self
+        self.scrollview!.isPagingEnabled = true
+        self.scrollview!.showsHorizontalScrollIndicator = false
+        self.contentView.addSubview(self.scrollview!)
+        
+        let colorView = UIView.init()
+        colorView.backgroundColor = self.color
+        self.scrollview?.addSubview(colorView)
+        colorView.tag = kColorViewTag
+        
+    }
+    
+    //必须要重载
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override open func layoutSubviews() {
+        let bounds = self.contentView.bounds
+        let pageWidth = bounds.size.width
+        self.scrollview!.frame = CGRect(x: 0, y: 0, width: pageWidth, height: bounds.size.height)
+        self.scrollview!.contentSize = CGSize(width: pageWidth * 2, height: bounds.size.height)
+        
+        let colorView = self.contentView.viewWithTag(kColorViewTag)
+        colorView?.frame = self.scrollview!.convert(bounds, from: self.contentView)
+        
+    }
     
 }
+
+///open 不能修饰protocol
+public protocol ScrollingCellDelegate : NSObjectProtocol{
+    @available(iOS 2.0, *)
+    func scrollingCellDidBeginPulling(_ cell: ScrollingCell)
+    
+    func scrollCell(_ cell: ScrollingCell, didChangePullOffset offset: Float)
+    
+    func scrollCellDidEndPulling(_ cell: ScrollingCell)
+}
+//public protocol UIScrollViewDelegate : NSObjectProtocol {
+//    
+//    
+//    
+//    optional public func scrollViewDidScroll(_ scrollView: UIScrollView) // any offset changes
--- NestedScrollView/ViewController.swift
+++ NestedScrollView/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate, UICollectionViewDataSource {
+    //MARK:
+    @IBOutlet weak var scrollViewOuter: UIScrollView!
+    
+    ///page 1 in scroll
+    weak var collectionView: UICollectionView!
+    ///page 2 in scroll
+    weak var viewSingle: UIView!
+    
+    //MARK:
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        p_configureOuterScrollView()
+        p_addSubviews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Private
+    fileprivate func p_configureOuterScrollView() -> Void {
+        let viewFrame = self.view.frame
+        self.scrollViewOuter.contentSize = CGSize(width: viewFrame.size.width * 2.0, height: viewFrame.size.height)
+        self.scrollViewOuter.isPagingEnabled = true
+        self.scrollViewOuter.delegate = self
+    }
+    
+    fileprivate func p_addSubviews() -> Void {
+        var viewFrame = self.view.frame
+        ///add collection view
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: viewFrame.size.width, height: 60)
+        flowLayout.minimumInteritemSpacing = 20
+        let collection = UICollectionView(frame: viewFrame, collectionViewLayout: flowLayout)
+        ///
+        collection.register(ScrollingCell.self, forCellWithReuseIdentifier: ScrollingCell.cellIdentifier)
+        self.scrollViewOuter.addSubview(collection)
+        self.collectionView = collection
+        self.collectionView.dataSource = self
+        
+        ///add single View
+        viewFrame.origin.x += viewFrame.size.width
+        let blueView = UIView(frame: viewFrame)
+        blueView.backgroundColor = UIColor.blue
+        self.scrollViewOuter.addSubview(blueView)
+        self.viewSingle = blueView
 
+    }
+    
+//    fileprivate func p_addSubviews() -> Void {
+//        var viewFrame = self.view.frame
+//        
+//        let redView = UIView(frame: viewFrame)
+//        redView.backgroundColor = UIColor.red
+//        self.scrollViewOuter.addSubview(redView)
+//        
+//        viewFrame.origin.x += viewFrame.size.width
+//        let blueView = UIView(frame: viewFrame)
+//        blueView.backgroundColor = UIColor.blue
+//        self.scrollViewOuter.addSubview(blueView)
+//    }
+    
+    
+    //MARK: UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ScrollingCell.cellIdentifier, for: indexPath) as! ScrollingCell
+        if indexPath.row % 2 == 0{
+            cell.color = UIColor.red
+        }
+        else if indexPath.row % 3 == 0{
+            cell.color = UIColor.green
+        }
+        else {
+            cell.color = UIColor.blue
+        }
+        return cell
+    }
 }
 
